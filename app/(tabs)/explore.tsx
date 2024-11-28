37d05c17a677444c024efb58a2f48e347b035c02
kishanshettyb <kishanqr@gmail.com>
2024-11-28 12:17:26 +0530

    fix: :bug: fix unused imports

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,11 +1,4 @@
-import { StyleSheet, Image, Platform, Text, View } from "react-native";
-
-import { Collapsible } from "@/components/Collapsible";
-import { ExternalLink } from "@/components/ExternalLink";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-import { IconSymbol } from "@/components/ui/IconSymbol";
+import { Text, View } from "react-native";
 
 export default function TabTwoScreen() {
   return (
@@ -14,16 +7,3 @@ export default function TabTwoScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  headerImage: {
-    color: "#808080",
-    bottom: -90,
-    left: -35,
-    position: "absolute",
-  },
-  titleContainer: {
-    flexDirection: "row",
-    gap: 8,
-  },
-});
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,4 @@
-import { Image, StyleSheet, Platform, Text, View } from "react-native";
-
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
@@ -12,22 +7,3 @@ export default function HomeScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-});
