49a218b0d84a0560b162dda85da3de2c389c51cb
kishanshettyb <kishanqr@gmail.com>
2024-12-02 23:28:12 +0530

    feat: :sparkles: add instagram size video size

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from "react";
 import { Image, Text, View } from "react-native";
 import Feather from "@expo/vector-icons/Feather";
 import HomeCarousel from "./Carousel";
+import VideoPost from "./VideoPost";
 
 const postData = [
   {
@@ -171,7 +172,92 @@ const PostCard = () => {
         } else if (item.postType == "reel") {
           return (
             <View className="mb-3" key={item.id}>
-              {/* <Text>Reel</Text> */}
+              <View className="relative">
+                <VideoPost />
+                <View className="px-4 z-50 absolute flex  flex-row py-2  w-full justify-between items-center">
+                  <View className="flex items-center justify-start flex-row gap-x-4">
+                    <View>
+                      <Image
+                        source={{ uri: item.profileImage }}
+                        className="w-[40px] h-[40px] rounded-full"
+                      />
+                    </View>
+                    <View>
+                      <Text className="text-sm font-semibold">
+                        {item.username}
+                      </Text>
+                    </View>
+                  </View>
+
+                  <View className="flex flex-row gap-x-2 items-center">
+                    <View className="border border-white rounded-lg px-4 py-2">
+                      <Text>Follow</Text>
+                    </View>
+                    <View>
+                      <Feather name="more-vertical" size={24} color="black" />
+                    </View>
+                  </View>
+                </View>
+                <View className="flex p-4 flex-row justify-between items-center">
+                  <View className="flex flex-row justify-start items-center gap-x-4">
+                    <View className="flex gap-x-2 justify-start items-center flex-row">
+                      <View>
+                        <Feather name="heart" size={24} color="black" />
+                      </View>
+                      <View>
+                        <Text>{item.likes}</Text>
+                      </View>
+                    </View>
+                    <View className="flex gap-x-2 justify-start items-center flex-row">
+                      <View>
+                        <Feather
+                          name="message-circle"
+                          size={24}
+                          color="black"
+                        />
+                      </View>
+                      <View>
+                        <Text>{item.comments}</Text>
+                      </View>
+                    </View>
+                    <View className="flex gap-x-2 justify-start items-center flex-row">
+                      <View>
+                        <Feather name="share-2" size={24} color="black" />
+                      </View>
+                      <View>
+                        <Text>{item.shares}</Text>
+                      </View>
+                    </View>
+                  </View>
+                  <View>
+                    <Feather name="bookmark" size={24} color="black" />
+                  </View>
+                </View>
+                <View className="px-4">
+                  <View className="flex flex-row justify-start items-center">
+                    <Image
+                      source={require("../assets/images/profile.png")}
+                      className="w-[20px] h-[20px] rounded-full border border-white"
+                    />
+                    <Image
+                      source={require("../assets/images/profile.png")}
+                      className="w-[20px] h-[20px] rounded-full -ml-2 border border-white"
+                    />
+                    <Text className="ml-2 opacity-70">
+                      Liked by <Text className="font-semibold">tangle</Text> and{" "}
+                      <Text className="font-semibold">others</Text>
+                    </Text>
+                  </View>
+                  <View>
+                    <Text numberOfLines={2} className="my-2">
+                      {item.description}
+                    </Text>
+                    <Text className="text-slate-500 text-sm">
+                      {item.createdDate}
+                    </Text>
+                  </View>
+                </View>
+              </View>
             </View>
           );
         } else {
