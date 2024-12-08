b65b73b2e83fe6b360976766994771d0085711dd
kishanshettyb <kishanqr@gmail.com>
2024-12-08 22:22:46 +0530

    fix: :recycle: auth code refactor

--- app/services/api/auth/auth.ts
+++ app/services/api/auth/auth.ts
@@ -1,10 +1,23 @@
-import { Register } from "@/app/types/auth";
 import axios from "axios";
 
+export type Register = {
+  username: string;
+  password: string;
+};
+
+export type Login = {
+  identifier: string;
+  password: string;
+};
+
 const axiosInstance = axios.create({
-	baseURL: process.env.EXPO_PUBLIC_BASE_URL
+  baseURL: process.env.EXPO_PUBLIC_BASE_URL,
 });
 
 export const registerUser = async (data: Register) => {
-	return await axiosInstance.post("register", data);
+  return await axiosInstance.post("register", data);
+};
+
+export const loginUser = async (data: Login) => {
+  return await axiosInstance.post("login", data);
 };
