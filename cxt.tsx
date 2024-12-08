93af50252c6d22691def154cb969eb01675eb30b
kishanshettyb <kishanqr@gmail.com>
2024-12-08 22:15:39 +0530

    feat: :recycle: refactor auth code

--- cxt.tsx
+++ cxt.tsx
@@ -1,50 +1,52 @@
-import { useContext, createContext, type PropsWithChildren } from "react";
+import { createContext, useContext, type PropsWithChildren } from "react";
 import { useStorageState } from "./useStorageState";
 import { router } from "expo-router";
 
-const AuthContext = createContext<{
-	signIn: () => void;
-	signOut: () => void;
-	session?: string | null;
-	isLoading: boolean;
-}>({
-	signIn: () => null,
-	signOut: () => null,
-	session: null,
-	isLoading: false
-});
+type AuthContextType = {
+  signIn: (data: { data: { jwt: string } }) => void;
+  signOut: () => void;
+  session: string | null;
+  isLoading: boolean;
+};
 
-// This hook can be used to access the user info.
-export function useSession() {
-	const value = useContext(AuthContext);
-	if (process.env.NODE_ENV !== "production") {
-		if (!value) {
-			throw new Error("useSession must be wrapped in a <SessionProvider />");
-		}
-	}
+const defaultContextValue: AuthContextType = {
+  signIn: () => {
+    throw new Error("signIn method not implemented.");
+  },
+  signOut: () => {
+    throw new Error("signOut method not implemented.");
+  },
+  session: null,
+  isLoading: false,
+};
 
-	return value;
-}
+const AuthContext = createContext<AuthContextType>(defaultContextValue);
 
-export function SessionProvider({ children }: PropsWithChildren) {
-	const [[isLoading, session], setSession] = useStorageState("session");
+export const useSession = (): AuthContextType => {
+  const context = useContext(AuthContext);
 
-	return (
-		<AuthContext.Provider
-			value={{
-				signIn: (data) => {
-					console.log(JSON.stringify(data));
-					setSession(data.data.jwt);
-					router.replace("/(app)/(tabs)");
-				},
-				signOut: () => {
-					setSession(null);
-				},
-				session,
-				isLoading
-			}}
-		>
-			{children}
-		</AuthContext.Provider>
-	);
-}
+  if (!context) {
+    throw new Error("useSession must be used within a <SessionProvider />.");
+  }
+
+  return context;
+};
+
+export const SessionProvider = ({ children }: PropsWithChildren) => {
+  const [[isLoading, session], setSession] = useStorageState<string | null>(
+    "session"
+  );
+
+  const signIn = (data: { data: { jwt: string } }) => {
+    setSession(data.data.jwt);
+    router.replace("/(app)/(tabs)");
+  };
+
+  const signOut = () => {
+    setSession(null);
+  };
+
+  const value = { signIn, signOut, session, isLoading };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
