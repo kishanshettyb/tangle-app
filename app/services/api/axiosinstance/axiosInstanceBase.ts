cb36465fc0258d15ced01ea616b52df7ea989c50
kishanshettyb <kishanqr@gmail.com>
2024-12-08 22:23:08 +0530

    fix: :bug: fix axios type errors

--- app/services/api/axiosinstance/axiosInstanceBase.ts
+++ app/services/api/axiosinstance/axiosInstanceBase.ts
@@ -1,58 +1,51 @@
-import { getAuthToken } from "../../../../cxt";
+import { useSession } from "@/cxt";
 import axios from "axios";
 
+// Base URL from environment variable
+const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL;
+
+// Function to get the authorization token
+const getAuthToken = (): string | null => {
+  const { session } = useSession();
+  return session || null;
+};
+
+// Create a default Axios instance
 export const axiosInstance = axios.create({
-	baseURL: process.env.EXPO_PUBLIC_BASE_URL
+  baseURL: BASE_URL,
 });
 
+// Add request interceptor to include authorization header
 axiosInstance.interceptors.request.use(
-	(config) => {
-		const token = getAuthToken();
-		if (token) {
-			config.headers.Authorization = `Bearer ${token}`;
-		}
-		// if (!config.headers["Content-Type"]) {
-		//     config.headers["Content-Type"] = "multipart/form-data";
-		// }
-
-		return config;
-	},
-	(error) => {
-		return Promise.reject(error);
-	}
+  (config) => {
+    const token = getAuthToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
+// Create an Axios instance for multipart/form-data requests
 export const axiosInstanceMultipart = axios.create({
-	baseURL: process.env.EXPO_PUBLIC_BASE_URL,
-	headers: {
-		"Content-Type": "multipart/form-data"
-	}
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
 });
 
-// const axiosInstance = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-//   headers: {
-//     Authorization: `Bearer ${token}`,
-//     'Content-Type': 'multipart/form-data'
-//   }
-// });
-
+// Add request interceptor for multipart instance
 axiosInstanceMultipart.interceptors.request.use(
-	(config) => {
-		const token = getAuthToken();
-		if (token) {
-			config.headers.Authorization = `Bearer ${token}`;
-		}
-
-		// if (!config.headers["Content-Type"]) {
-		//     config.headers["Content-Type"] = "multipart/form-data";
-		// }
-
-		return config;
-	},
-	(error) => {
-		return Promise.reject(error);
-	}
+  (config) => {
+    const token = getAuthToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
-// Add a default export
+
+// Export the default Axios instance
 export default axiosInstance;
