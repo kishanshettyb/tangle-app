b2ce32fcdc5e2543a43e0e0ad31366de8db58988
kishanshettyb <kishanqr@gmail.com>
2024-12-15 13:35:03 +0530

    feat: :sparkles: add new state to the auth store

--- app/store/authStore.ts
+++ app/store/authStore.ts
@@ -2,12 +2,16 @@ import { create } from "zustand";
 
 interface AuthState {
   email: string;
+  username: string;
   setEmail: (email: string) => void;
+  setUsername: (username: string) => void;
 }
 
 const useAuthStore = create<AuthState>((set) => ({
   email: "",
+  username: "",
   setEmail: (email) => set({ email }),
+  setUsername: (username) => set({ username }),
 }));
 
 export default useAuthStore; // Default export added
