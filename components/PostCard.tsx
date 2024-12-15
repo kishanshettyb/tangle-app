33832aa9dedf6bb0975b0a948afb9241759743b1
kishanshettyb <kishanqr@gmail.com>
2024-12-15 13:37:48 +0530

    feat: :sparkles: add new tabs for profile page

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -7,115 +7,124 @@ import PostHeader from "./PostHeader";
 import PostFooter from "./PostFooter";
 
 const postData = [
-	{
-		id: 1,
-		postType: "singleImage",
-		username: "Sofia",
-		profileImage: "https://i.ibb.co/gzdYtSr/img-3.png",
-		description:
-			"🌿 Nature's Magic 🌿 Take a deep breath and let nature heal your soul. From the rustling leaves to the golden sunsets, every moment outdoors is a gentle reminder of life's beauty. 🌳✨ #NatureLovers #Serenity #NatureHealing",
-		createdDate: "01-12-2024",
-		likedBy: "username",
-		likes: 55,
-		comments: 77,
-		shares: 98,
-		bookmarked: false,
-		postAssets: [
-			{
-				id: 1,
-				imageUrl: "https://i.ibb.co/Z2gFqpQ/aaron-burden-d-XYE1d08-Bi-Y-unsplash.jpg"
-			}
-		]
-	},
-	{
-		id: 2,
-		username: "Omar",
-		postType: "reel",
-		profileImage: "https://i.ibb.co/Y3ZKH4C/img-1.png",
-		description:
-			"In the whisper of the wind and the song of the birds lies the melody of peace. Step outside, feel the earth, and let nature inspire your heart. 🌼🌞#NatureVibes #PeacefulMoments #NatureInspires",
-		createdDate: "01-12-2024",
-		likedBy: "username",
-		likes: 3300,
-		comments: 2100,
-		shares: 900,
-		bookmarked: false,
-		postAssets: [
-			{
-				id: 1,
-				videoUrl: "https://videos.pexels.com/video-files/25254351/11900288_1080_1920_30fps.mp4"
-			}
-		]
-	},
-	{
-		id: 3,
-		username: "Elena",
-		postType: "carousel",
-		profileImage: "https://i.ibb.co/N9YnGhP/img-2.png",
-		description:
-			"🚗 Driven to Explore 🚗 Life's a journey, and every car has a story. Whether it's the thrill of the open road or the comfort of a smooth ride, your car isn't just a vehicle—it's your freedom. 🛣️✨#CarLovers #DriveYourDreams #OnTheRoad",
-		createdDate: "01-12-2024",
-		likedBy: "username",
-		likes: 55,
-		comments: 87,
-		shares: 23,
-		bookmarked: false,
-		postAssets: [
-			{
-				id: 1,
-				imageUrl: "https://i.ibb.co/p4S3XjB/hayes-potter-mo-Mm-TIfd-Ga4-unsplash.jpg"
-			},
-			{
-				id: 2,
-				imageUrl: "https://i.ibb.co/94Vhz2n/connor-lunsford-Em-Zn-Kn-Vt4qs-unsplash.jpg"
-			},
-			{
-				id: 3,
-				imageUrl: "https://i.ibb.co/FVH1svd/hayes-potter-te-TQy-Obj-Xs-unsplash.jpg"
-			},
-			{
-				id: 4,
-				imageUrl: "https://i.ibb.co/XDw2CYJ/peter-broomfield-m3m-ln-R90u-M-unsplash.jpg"
-			}
-		]
-	}
+  {
+    id: 1,
+    postType: "singleImage",
+    username: "Sofia",
+    profileImage: "https://i.ibb.co/gzdYtSr/img-3.png",
+    description:
+      "🐾 Beach Day Bliss 🌊]Nothing beats the pure joy of a dog running free on the beach! 🐕✨From splashing in the waves to chasing seagulls this pup is living their best life. 😍📍 Perfect day. Perfect vibes. Do you take your dog to the beach? Drop a 🐶 in the comments if you’re a dog lover! BeachDay DogAtTheBeach #PuppyLove #DogLife #DogsOfInstagram #OceanVibes",
+    createdDate: "01-12-2024",
+    likedBy: "username",
+    likes: 55,
+    comments: 77,
+    shares: 98,
+    bookmarked: false,
+    postAssets: [
+      {
+        id: 1,
+        imageUrl:
+          "https://i.ibb.co/5rrdcch/oscar-sutton-yihla-RCCvd4-unsplash.jpg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    username: "Omar",
+    postType: "reel",
+    profileImage: "https://i.ibb.co/Y3ZKH4C/img-1.png",
+    description:
+      "In the whisper of the wind and the song of the birds lies the melody of peace. Step outside, feel the earth, and let nature inspire your heart. 🌼🌞#NatureVibes #PeacefulMoments #NatureInspires",
+    createdDate: "01-12-2024",
+    likedBy: "username",
+    likes: 3300,
+    comments: 2100,
+    shares: 900,
+    bookmarked: false,
+    postAssets: [
+      {
+        id: 1,
+        videoUrl:
+          "https://videos.pexels.com/video-files/25254351/11900288_1080_1920_30fps.mp4",
+      },
+    ],
+  },
+  {
+    id: 3,
+    username: "Elena",
+    postType: "carousel",
+    profileImage: "https://i.ibb.co/N9YnGhP/img-2.png",
+    description:
+      "🚗 Driven to Explore 🚗 Life's a journey, and every car has a story. Whether it's the thrill of the open road or the comfort of a smooth ride, your car isn't just a vehicle—it's your freedom. 🛣️✨#CarLovers #DriveYourDreams #OnTheRoad",
+    createdDate: "01-12-2024",
+    likedBy: "username",
+    likes: 55,
+    comments: 87,
+    shares: 23,
+    bookmarked: false,
+    postAssets: [
+      {
+        id: 1,
+        imageUrl:
+          "https://i.ibb.co/p4S3XjB/hayes-potter-mo-Mm-TIfd-Ga4-unsplash.jpg",
+      },
+      {
+        id: 2,
+        imageUrl:
+          "https://i.ibb.co/94Vhz2n/connor-lunsford-Em-Zn-Kn-Vt4qs-unsplash.jpg",
+      },
+      {
+        id: 3,
+        imageUrl:
+          "https://i.ibb.co/FVH1svd/hayes-potter-te-TQy-Obj-Xs-unsplash.jpg",
+      },
+      {
+        id: 4,
+        imageUrl:
+          "https://i.ibb.co/XDw2CYJ/peter-broomfield-m3m-ln-R90u-M-unsplash.jpg",
+      },
+    ],
+  },
 ];
 const PostCard = () => {
-	return (
-		<>
-			{postData.map(function (item) {
-				if (item.postType == "singleImage") {
-					return (
-						<View className="mb-6" key={item.id}>
-							<PostHeader data={item} />
-							<View>
-								<Image className="w-full h-[300px] object-cover" source={{ uri: item.postAssets[0].imageUrl }} />
-							</View>
-							<PostFooter data={item} />
-						</View>
-					);
-				} else if (item.postType == "reel") {
-					return (
-						<View className="mb-3" key={item.id}>
-							<View className="relative">
-								<VideoPost data={item.postAssets[0].videoUrl} />
-								<PostHeader data={item} />
-								<PostFooter data={item} />
-							</View>
-						</View>
-					);
-				} else {
-					return (
-						<View key={item.id}>
-							<PostHeader data={item} />
-							<HomeCarousel items={item.postAssets} />
-							<PostFooter data={item} />
-						</View>
-					);
-				}
-			})}
-		</>
-	);
+  return (
+    <>
+      {postData.map(function (item) {
+        if (item.postType == "singleImage") {
+          return (
+            <View className="mb-6" key={item.id}>
+              <PostHeader data={item} />
+              <View>
+                <Image
+                  className="w-full h-[300px] object-cover"
+                  source={{ uri: item.postAssets[0].imageUrl }}
+                />
+              </View>
+              <PostFooter data={item} />
+            </View>
+          );
+        } else if (item.postType == "reel") {
+          return (
+            <View className="mb-3" key={item.id}>
+              <View className="relative">
+                <VideoPost data={item.postAssets[0].videoUrl} />
+                <PostHeader data={item} />
+                <PostFooter data={item} />
+              </View>
+            </View>
+          );
+        } else {
+          return (
+            <View key={item.id}>
+              <PostHeader data={item} />
+              <HomeCarousel items={item.postAssets} />
+              <PostFooter data={item} />
+            </View>
+          );
+        }
+      })}
+    </>
+  );
 };
 
 export default PostCard;
---
+++ components/Profile/ProfileCard.tsx
@@ -0,0 +1,54 @@
+import Feather from "@expo/vector-icons/Feather";
+import { Link } from "expo-router";
+import React, { FunctionComponent } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+
+const ProfileCard = ({ userName }) => {
+  return (
+    <View className="border border-slate-100  rounded-2xl">
+      <View className="flex flex-row justify-between items-center  p-4">
+        <View className="border-2 w-[90px]  h-[90px] rounded-full flex justify-center items-center  border-purple-700">
+          <TouchableOpacity className="relative">
+            <Image
+              source={require("../../assets/images/icon.png")}
+              className="w-[80px] h-[80px] rounded-full  "
+            />
+            <View className="absolute bottom-0 -right-3 shadow border border-slate-50 shadow-slate-200 w-[30px] h-[30px] flex justify-center items-center rounded-full bg-white">
+              <Feather name="camera" size={16} color="black" />
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text className="text-2xl font-semibold text-center">2</Text>
+          <Text className="text-lg  text-center">post</Text>
+        </View>
+        <View>
+          <Text className="text-2xl font-semibold text-center">5</Text>
+          <Text className="text-lg  text-center">followers</Text>
+        </View>
+        <View>
+          <Text className="text-2xl font-semibold text-center">7</Text>
+          <Text className="text-lg  text-center">following</Text>
+        </View>
+      </View>
+      <View className="px-6 pb-4">
+        <Text className="font-semibold text-xl">{userName}</Text>
+        <Text className="text-md my-1" numberOfLines={2}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias soluta
+          dolor rerum! Excepturi dolor earum provident enim molestiae, explicabo
+          eligendi perferendis blanditiis obcaecati officia eveniet nihil
+          laudantium tempore. Harum, error.
+        </Text>
+        <Link
+          className="text-purple-600"
+          href="https://www.makemyonlinestore.in"
+        >
+          <Feather name="link" size={14} color="#9333ea" />{" "}
+          <Text className="text-md">links</Text>
+        </Link>
+      </View>
+    </View>
+  );
+};
+
+export default ProfileCard;
